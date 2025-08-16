0b56966fe4f1410fdb840b30e1f6bfb82feccb5d
Rashel Hossen <codebyrashel@gmail.com>
2025-08-16 15:13:05 +0600

    Problem Task 1 Solved

---
+++ Js-Problem/problem_1.js
@@ -0,0 +1,30 @@
+/** Task-1
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result.
+
+Sample Input:
+1000
+700
+
+Sample Output:
+300
+
+*/
+
+
+// Answer:
+
+const balance = 1000;
+let oranges = 200;
+let apples = 300;
+
+let totalPrice = oranges + apples;
+
+const returnMoney = balance - totalPrice;
+
+console.log(returnMoney);
