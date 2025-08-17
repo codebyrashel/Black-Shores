c9fd28f20a26963e52aeb12f23e29b306476aabf
Rashel Hossen <codebyrashel@gmail.com>
2025-08-17 10:34:09 +0600

    Task 3 and 4 Solved

--- Js-Problem/Js_condtional_task.js
+++ Js-Problem/Js_condtional_task.js
@@ -49,6 +49,80 @@ else{
 }
 
 
+/***
+
+Grade Calculator
+
+Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+
+    A: 90-100
+    B: 80-89
+    C: 70-79
+    D: 60-69
+    F: 0-59
+
+***/
 
 
+// Answer:
+
+let score = 670
+
+if (score >= 90 && score <= 100) {
+    console.log('Grade A')
+}
+else if (score >= 80 && score <= 89) {
+    console.log('Grade B')
+}
+else if (score >= 70 && score <= 79) {
+    console.log('Grade C')
+}
+else if (score >= 60 && score <= 69) {
+    console.log('Grade D')
+}
+else if (score >= 0 && score <= 59) {
+    console.log('Grade F')
+}
+else {
+    console.log('Invalid score')
+}
+
+
+
+/***
+
+if you get more then 80 then inside your friend score. 
+If your friend get more than 80. then go for a lunch. 
+if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+
+
+// Answer :
+
+let myScore = 91;
+let myFriendsScore = 39;
+
+if(myScore > 80){
+    if(myFriendsScore > 80){
+        console.log('Yay! Let\'s go for lunch')
+    }
+    else if(myFriendsScore >= 60){
+        console.log('Good Luck for next time, Buddy!')
+    }
+    else if(myFriendsScore >= 40){
+        console.log('Keep your friend\'s message unseen')
+    }
+    else{
+        console.log('I\'m blocking you so, Don\'t Contact me Again')
+    }
+}
+else{
+    console.log('Go Home, Sleep and act sad')
+}
 
