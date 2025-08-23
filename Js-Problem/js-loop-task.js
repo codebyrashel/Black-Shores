ee1cc4ceb4c3880b7b30c2740b97fc6fb9990c17
Rashel Hossen <codebyrashel@gmail.com>
2025-08-23 16:25:23 +0600

    Js loop task all complete but last 2 need to review again

--- Js-Problem/js-loop-task.js
+++ Js-Problem/js-loop-task.js
@@ -279,8 +279,8 @@ Generate a multiplication table for number 9
 
 
 let multiplySecond = 9
-for(let i = 1; i <= 9; i++){
-    console.log(i + ' x ' + multiplySecond + ' = ' (i * multiplySecond));
+for(let i = 1; i <= 10; i++){
+    console.log(i + ' x ' + multiplySecond + ' = ' + (i * multiplySecond));
 }
 
 // Using while loop method
@@ -349,6 +349,33 @@ for(let i = 1; i <= 40; i++){
 display odd number from 55 to 85 and skip the numbers divisible by 5.
 */
 
+//Answer:
+
+for(let i = 55; i <= 85; i++){
+    if(i % 2 !== 0){
+        if( i % 5 === 0){
+            continue
+        }
+        console.log(i);
+    }
+}
+
+// using while loop
+
+let n = 55;
+while (n <= 85) {
+    if (n % 2 === 0 || n % 5 === 0) {
+        n++;
+        continue;
+    }
+    console.log(n);
+    n++;
+}
+
+// more shorter way without using nested if condition
+
+// Failed
+
 
 /* 
 
@@ -356,12 +383,46 @@ Write a loop 1 to 200. Use break to exit the loop once you find 100.
 
 */
 
+// Answer:
+
+for(let i = 1; i <= 200; i++){
+    console.log(i);
+    if(i === 100){
+        break
+    }
+}
 
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
 
+// Answer :
+
+let n = 1;
+let sum = 0;
+
+while (true) {
+    sum += n;
+    if (sum >= 100) {
+        break;  // stop when sum reaches or exceeds 100
+    }
+    n++;
+}
+
+console.log("Final n:", n);
+console.log("Final sum:", sum);
+
+
 
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+
+// Answer:
+
+for (let i = 1; i <= 100; i++) {
+    if (i > 1 && Number.isInteger(Math.sqrt(i))) {
+        break;
+    }
+    console.log(i);
+}
