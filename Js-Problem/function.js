d39bcd92f69e9b6158bb8206472bb61c049310e1
Rashel Hossen <codebyrashel@gmail.com>
2025-09-11 14:36:16 +0600

    Js function practice task all solved

--- Js-Problem/function.js
+++ Js-Problem/function.js
@@ -224,3 +224,19 @@ console.log(vowelCount);
 
 // Task-5
 // Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+
+// Answer:
+
+function odd_even(intValue){
+    if(intValue % 2 === 0){
+        return "even"
+    }
+    else{
+        return "odd"
+    }
+}
+
+// odd_even(2);
+
+const checkNumber = odd_even(5);
+console.log(checkNumber);
