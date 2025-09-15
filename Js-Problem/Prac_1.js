feee0edb53adacb9d02e062fce8c8f586a3e4e1d
Rashel Hossen <codebyrashel@gmail.com>
2025-09-15 19:40:05 +0600

    Additional sub task solved with new gap indentification about hashmap now additional 2 more sub task will be solved to learn about hashmap and solve the task

--- Js-Problem/Prac_1.js
+++ Js-Problem/Prac_1.js
@@ -19,7 +19,7 @@ console.log(temperature);
 
 let numbers = [1, 2, 2, 3, 4, 4, 4, 5, 1, 2];
 
-let count = {};
+let count = {}; //hashmap
 
 for (let num of numbers) {
   if (count[num]) {
@@ -33,6 +33,71 @@ for (let num of numbers) {
 for (let key in count) {
   console.log(`Number ${key} is repeated ${count[key]} time(s).`);
 }
+
+// Sub-task 1:
+
+// You are given an array of numbers.
+// Find the first number that is repeated in the array.
+
+// Example:
+// Input: [3, 5, 2, 3, 7, 5]
+// Output: The first repeated number is 3
+
+
+let array = [3, 5, 2, 3, 7, 5];
+
+let empty = {};
+
+for(num of array){
+    // console.log(empty);
+    if (empty[num]){
+        // console.log(empty);
+        // empty[num] += 1;
+        console.log("The first repeated number is " + num);
+        break
+    }
+    else{
+        // console.log(empty);
+        empty[num] = 1;
+    }
+}
+
+// Sub-task 2:
+
+// You are given an array of numbers.
+// Find the number that is repeated the most and print how many times it occurs.
+
+// Example:
+// Input: [4, 1, 4, 2, 4, 3, 2, 2, 2]
+// Output: The number 4 is repeated 3 times (most repeated)
+
+let repeatedNumbers = [4, 1, 4, 2, 4, 3, 2, 2, 2];
+
+let stored = {};
+
+// Step 1: Count frequencies
+for (let num of repeatedNumbers) {
+    if (stored[num]) {
+        stored[num] += 1;
+    } else {
+        stored[num] = 1; // cleaner than "+1"
+    }
+}
+
+console.log("Frequencies:", stored); // just to see the counts
+
+// Step 2: Find the most repeated number
+let maxCount = 0;
+let mostRepeated;
+
+for (let key in stored) {
+    if (stored[key] > maxCount) {
+        maxCount = stored[key];
+        mostRepeated = key;
+    }
+}
+
+console.log(`The number ${mostRepeated} is repeated ${maxCount} times (most repeated).`);
 
 
 // sample-input: numbers = [5,6,11,12,98, 5]
--- Js-Problem/test.js
+++ Js-Problem/test.js
@@ -1,33 +1,86 @@
-// Task-5
-// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+// Task-2:
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
 
-// Answer:
+// Task-2: Count how many times each number is repeated in the array
 
-function odd_even(intValue){
-    if(intValue % 2 === 0){
-        return "even"
+let numbers = [1, 2, 2, 3, 4, 4, 4, 5, 1, 2];
+
+let count = {}; //hashmap
+
+for (let num of numbers) {
+  if (count[num]) {
+    count[num] += 1;  // If number already exists, increase count
+  } else {
+    count[num] = 1;   // Otherwise, initialize with 1
+  }
+}
+
+// Print result
+for (let key in count) {
+  console.log(`Number ${key} is repeated ${count[key]} time(s).`);
+}
+
+// Sub-task 1:
+
+// You are given an array of numbers.
+// Find the first number that is repeated in the array.
+
+// Example:
+// Input: [3, 5, 2, 3, 7, 5]
+// Output: The first repeated number is 3
+
+
+let array = [3, 5, 2, 3, 7, 5];
+
+let empty = {};
+
+for(num of array){
+    // console.log(empty);
+    if (empty[num]){
+        // console.log(empty);
+        // empty[num] += 1;
+        console.log("The first repeated number is " + num);
+        break
     }
     else{
-        return "odd"
+        // console.log(empty);
+        empty[num] = 1;
     }
 }
 
-// odd_even(2);
+// Sub-task 2:
 
-const checkNumber = odd_even(5);
-console.log(checkNumber);
+// You are given an array of numbers.
+// Find the number that is repeated the most and print how many times it occurs.
 
+// Example:
+// Input: [4, 1, 4, 2, 4, 3, 2, 2, 2]
+// Output: The number 4 is repeated 3 times (most repeated)
 
-function Twoodd(year){
-    for(const leap of year){
-        if(leap % 4 === 0){
-            return true
-        }
-        else{
-            return false
-        }
+let repeatedNumbers = [4, 1, 4, 2, 4, 3, 2, 2, 2];
+
+let stored = {};
+
+// Step 1: Count frequencies
+for (let num of repeatedNumbers) {
+    if (stored[num]) {
+        stored[num] += 1;
+    } else {
+        stored[num] = 1; // cleaner than "+1"
     }
 }
 
-const lipi = Twoodd(2100);
-console.log(lipi);
+console.log("Frequencies:", stored); // just to see the counts
+
+// Step 2: Find the most repeated number
+let maxCount = 0;
+let mostRepeated;
+
+for (let key in stored) {
+    if (stored[key] > maxCount) {
+        maxCount = stored[key];
+        mostRepeated = key;
+    }
+}
+
+console.log(`The number ${mostRepeated} is repeated ${maxCount} times (most repeated).`);
