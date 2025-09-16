98f0acffcad68bdddc153a1c1dec757606c07b42
Rashel Hossen <codebyrashel@gmail.com>
2025-09-16 22:03:48 +0600

    Js practise problem task 4 solved

--- Js-Problem/Prac_1.js
+++ Js-Problem/Prac_1.js
@@ -222,6 +222,28 @@ const vowelsCount = str => [...str.toLowerCase()].filter(c => "aeiou".includes(c
 // Task-4:
 // Write a function to find the longest word in a given string.
 
+let sentence = "I don't know Programming";
+
+function findLongestWord(inputString) {
+    let words = inputString.split(" ");
+    let longestWord = words[0];
+
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+
+    return longestWord;
+}
+
+const result = findLongestWord(sentence);
+
+console.log(result);
+
+// console.log(findLongestWord(sentence));
+
+
 // sample-input: I am learning Programming to become a programmer
 
 // sample-output: Programming
--- Js-Problem/test.js
+++ Js-Problem/test.js
@@ -1,33 +1,23 @@
-// Task-3:
-// Write a function to count the number of vowels in a string.
+// Task-4:
+// Write a function to find the longest word in a given string.
 
-function vowelsCount (count){
-    let vowelCount = 0;
-    for(let char of count){
-        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
-            vowelCount ++; 
+let sentence = "I don't know Programming";
+
+function findLongestWord(inputString) {
+    let words = inputString.split(" ");
+    let longestWord = words[0];
+
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
         }
     }
-    return vowelCount;
+
+    return longestWord;
 }
 
-const normalString = vowelsCount("How quickly daft jumping zebras vex my boy");
+const result = findLongestWord(sentence);
 
-console.log(normalString);
+console.log(result);
 
-// With few failed attempt This one is mine as I solved it with the knowledge I had, Now below will be the solution given by the AI
-
-// Shorter version 
-
-function vowelsCount(str) {
-    const vowels = "aeiou";
-    let count = 0;
-    for (let char of str.toLowerCase()) {
-        if (vowels.includes(char)) count++;
-    }
-    return count;
-}
-
-// Another one using one line 
-
-const vowelsCount = str => [...str.toLowerCase()].filter(c => "aeiou".includes(c)).length;
+// console.log(findLongestWord(sentence));
